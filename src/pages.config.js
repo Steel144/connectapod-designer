c85588887be819885538700c5ab27fd18ff495a4
Les McKenzie <lesliejohnmckenzie@gmail.com>
2026-03-02 09:50:02 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -48,6 +48,7 @@
  * The mainPage value must match a key in the PAGES object exactly.
  */
 import Configurator from './pages/Configurator';
+import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
@@ -57,4 +58,5 @@ export const PAGES = {
 export const pagesConfig = {
     mainPage: "Configurator",
     Pages: PAGES,
+    Layout: __Layout,
 };
