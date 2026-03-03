b13bfb1b31c9cb547fe27c2e5fabe8069539d4b3
Les McKenzie <lesliejohnmckenzie@gmail.com>
2026-03-03 02:46:43 +0000

    File changes

--- src/pages/Configurator.jsx
+++ src/pages/Configurator.jsx
@@ -346,7 +346,7 @@ export default function Configurator() {
             <p className="text-[11px] text-gray-400 mt-0.5">Expand a category · drag to place</p>
           </div>
           <div className="flex-1 overflow-y-auto p-3">
-            <ModulePanel onDragStart={handleDragStart} onDragEnd={handleDragEnd} />
+            <ModulePanel onDragStart={handleDragStart} onDragEnd={handleDragEnd} selectedWall={selectedWall} placedModules={placedModules} />
           </div>
         </div>
 
