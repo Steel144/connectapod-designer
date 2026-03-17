5a2fbc301267b592fb527f7a4c1d232f22082a95
Les McKenzie <lesliejohnmckenzie@gmail.com>
2026-03-17 08:09:40 +0000

    File changes

--- src/components/configurator/PrintablePlansSheet.jsx
+++ src/components/configurator/PrintablePlansSheet.jsx
@@ -46,7 +46,7 @@ export default function PrintablePlansSheet({ placedModules, onClose }) {
 
          {/* Header with logo */}
           <div className="flex items-center px-6 pt-4 pb-2 border-b" style={{ borderColor: "#F15A22" }}>
-            <img src="https://media.base44.com/images/public/69a55c0c222e61cb3fbc417c/61a42fba4_ConnectapodArchLogo-01.png" alt="connectapod" style={{ height: "40px", width: "auto" }} />
+            <img src="https://media.base44.com/images/public/69a55c0c222e61cb3fbc417c/201470147_ConnectapodArchLogo-01.png" alt="connectapod" style={{ height: "40px", width: "auto" }} />
             <span style={{ marginLeft: "8mm", color: "#666", fontSize: "12pt" }}>Floor Plan</span>
           </div>
 
